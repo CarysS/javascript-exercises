3e7045c983e8e8389f959b3dcd0bf160755e9df4
CarysS <39626099+CarysS@users.noreply.github.com>
2018-05-25 09:21:21 -0500

    Create ex2.js

---
+++ ex2.js
@@ -0,0 +1,11 @@
+//given 2 arrays this function returns an average of their lengths
+
+function computeAverageLengthOfWords(word1, word2) {
+  // your code here
+  return (word1.length + word2.length)/2;
+}
+
+
+
+var output = computeAverageLengthOfWords('code', 'programs');
+console.log(output);
