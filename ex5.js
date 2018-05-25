cf4b6f243b05197bdc0b9866f8f90e119546b366
CarysS <39626099+CarysS@users.noreply.github.com>
2018-05-25 09:24:58 -0500

    Create ex5.js

---
+++ ex5.js
@@ -0,0 +1,14 @@
+//Given 3 words, "findMinLengthOfThreeWords" returns the length of the shortest word.
+
+function findMinLengthOfThreeWords(word1, word2, word3) {
+  // your code here
+  if (word1.length < word2.length && word3.length){
+    return (word1.length)}
+  else if (word2.length < word1.length && word3.length){
+    return (word2.length)}
+  else if (word3.length < word1.length && word2.length){
+    return (word3.length)}
+}
+
+var output = findMinLengthOfThreeWords('a', 'be', 'see');
+console.log(output);
